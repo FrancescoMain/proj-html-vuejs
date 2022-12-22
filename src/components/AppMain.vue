5b99991634f3c0695ef5efbc71233a88cc34d66a
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 17:00:36 +0100

    importato headache Free

---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script >
+import HeadacheFree from './HeadacheFree.vue'
+
+
+export default {
+
+    name: "AppMain",
+    components: {
+        HeadacheFree
+    }
+
+}
+
+
+
+</script>
+
+<template>
+    <main>
+        <!-- headache free -->
+        <HeadacheFree />
+    </main>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
