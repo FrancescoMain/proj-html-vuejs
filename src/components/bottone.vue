4d36f15e02244dd74df8f8124f2ffb89cec033ad
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 11:49:42 +0100

    creato componente bottone

---
+++ src/components/bottone.vue
@@ -0,0 +1,32 @@
+<script >
+
+</script>
+
+<template>
+    <a href="
+">
+        <div class="bottone">
+            FREE QUOTE
+        </div>
+    </a>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.bottone {
+    background-color: $denim;
+    color: $white;
+    font-weight: 700;
+    border-radius: 20px;
+    padding: 9px 25px;
+    align-self: baseline;
+
+    &:hover {
+        color: $shark;
+    }
+
+}
+</style>
