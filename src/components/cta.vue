c22bd9253392671da6e455082cec205d1e748e5d
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 12:18:14 +0100

    creato cta component

---
+++ src/components/cta.vue
@@ -0,0 +1,40 @@
+<script >
+
+</script>
+
+<template>
+
+    <div class="wrap">
+        <div class="container">
+            <div class="row  justify-content-center align-items-center text-center">
+                <div class="col-5">
+
+                    <a href=""> <font-awesome-icon class="me-2" icon="fa-regular fa-paper-plane" />GET A FREE QUOTE
+                        ONLINE
+                        NOW </a>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.wrap {
+    color: $white;
+    background-color: $denim;
+    height: 110px;
+    font-size: 1.2rem;
+
+    .row {
+        height: 110px;
+
+
+    }
+
+}
+</style>
