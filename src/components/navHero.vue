7b1aab47e1f10dd111b1e615aa5d25309450d961
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 11:22:53 +0100

    creata struttrua base navHero

---
+++ src/components/navHero.vue
@@ -0,0 +1,38 @@
+<script >
+import navBar from './navBar.vue'
+import hero from './hero.vue'
+
+
+export default {
+
+    name: "navHero",
+    components: {
+        navBar,
+        hero
+    }
+
+}
+
+
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <!-- nav -->
+        <navBar />
+        <!-- hero -->
+        <hero />
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+    background-image: url(../../public/img/avada-movers-homeherobackground-final.jpg);
+    height: 800px;
+    background-size: 2650px;
+    background-position-x: -302px;
+    background-position-y: -48px;
+}
+</style>
