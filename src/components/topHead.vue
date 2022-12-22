e6389ea7abe79d335b7fde8f28eda10e502dea3c
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 10:11:24 +0100

    creata struttura con bs

---
+++ src/components/topHead.vue
@@ -0,0 +1,26 @@
+<script >
+
+</script>
+
+<template>
+
+    <div class="wrap">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col"></div>
+                <div class="col"></div>
+            </div>
+        </div>
+
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.wrap {
+    background-color: $lima;
+    height: 47px;
+}
+</style>
