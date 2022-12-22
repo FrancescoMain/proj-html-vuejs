e523e453ae4d6c8e159330d56b759f41a72f88dc
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 18:46:28 +0100

    creato componente free quote

---
+++ src/components/FreeQuote.vue
@@ -0,0 +1,66 @@
+<script >
+import Bottone from './Bottone.vue'
+
+import { store } from "../store.js";
+
+
+export default {
+
+    name: "FreeQuote",
+    components: {
+        Bottone
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="effect">
+        <div class="wrapper">
+            <div class="text-cont">
+                <h4>{{ store.freeQuoteTexts.topTitle }}</h4>
+                <h2>{{ store.freeQuoteTexts.title }}</h2>
+                <div class="line"></div>
+                <p>{{ store.freeQuoteTexts.text }}</p>
+                <bottone :data="store.freeQuoteBut" />
+            </div>
+        </div>
+    </div>
+
+
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+
+
+.wrapper {
+    background-image:
+        url(../../public/img/avada-movers-servicetwophoto-final.jpg);
+    height: 800px;
+    background-repeat: no-repeat;
+    background-size: 1900px;
+    background-position-y: -200px;
+    position: relative;
+
+    .text-cont {
+        height: 424px;
+        width: 619px;
+        position: absolute;
+        top: 219px;
+        right: 245px;
+        background-color: $lima;
+        border: 14px solid $border;
+        color: $white;
+    }
+}
+</style>
