7de12d193e9ba0f9d9271dd8a23be79dcebd8ecf
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 10:32:34 +0100

    importata icona PhoneFlip

--- src/main.js
+++ src/main.js
@@ -9,10 +9,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faPhone)
+library.add(faPhoneFlip)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
