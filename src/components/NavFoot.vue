20c48ef9d77f5055d1453585a4a19af19515a38c
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 15:10:13 +0100

    aggiunto nav foot

---
+++ src/components/NavFoot.vue
@@ -0,0 +1,73 @@
+<script >
+
+
+export default {
+
+    name: "NavFoot",
+    props: {
+        title: String,
+        links: Array,
+    },
+
+
+}
+
+</script>
+
+<template>
+
+
+
+    <div class="nav-cont">
+        <h3>{{ title }}</h3>
+        <ul>
+            <li v-for="link in links" :key="link.id" v-show="link.footer">
+                <a :href="link.href">{{ link.title }}</a>
+            </li>
+            <li>
+                <a href="">Free Quote</a>
+            </li>
+        </ul>
+    </div>
+
+
+
+
+
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.nav-cont {
+    margin-bottom: 20px;
+}
+
+h3 {
+    font-size: 1.2rem;
+    font-weight: 700;
+    padding-bottom: 20px;
+}
+
+ul {
+    color: $shark ;
+    margin: 0;
+    list-style: url('../../public/img/chevron-right-solid.svg');
+
+    li {
+        line-height: 40px;
+
+        a {
+            color: $shark;
+
+            &:hover {
+                color: $lima-dark-vibrant;
+            }
+        }
+    }
+
+
+}
+</style>
