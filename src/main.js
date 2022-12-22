5a5ad874dd38bacb48fdfd3f7b13749edf718a06
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 12:17:57 +0100

    importata libreria regular e nuova icona

--- src/main.js
+++ src/main.js
@@ -10,10 +10,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
 import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'
 import { faFacebookF, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faPhoneFlip, faFacebookF, faTwitter, faInstagram, faYoutube)
+library.add(faPhoneFlip, faFacebookF, faTwitter, faInstagram, faYoutube, faPaperPlane)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
