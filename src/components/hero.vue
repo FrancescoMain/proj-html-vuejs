c5db15a21e6b853bedc7a8d64a6d79c453f6ffed
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 12:02:09 +0100

    creato componente hero

---
+++ src/components/hero.vue
@@ -0,0 +1,39 @@
+<script >
+
+</script>
+
+<template>
+    <div class="cont">
+        <h4>MOVING WHITOUT THE HASSLE</h4>
+        <h1>WE MAKE IT SIMPLE</h1>
+        <h4>PROFESSIONAL SEVICE WITH HIGH QUALITY AND CUSTOMER SATISFACTION</h4>
+
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+
+.cont {
+    margin-top: 270px;
+
+    h1,
+    h4 {
+        text-align: center;
+        font-weight: 700;
+    }
+
+    h4 {
+        color: $dove-gray;
+        font-size: 0.8rem;
+
+    }
+
+    h1 {
+        font-size: 3.5rem;
+    }
+}
+</style>
