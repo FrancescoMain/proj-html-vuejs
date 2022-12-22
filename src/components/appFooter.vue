43e86a608cbd2481c88bfc723b64771141413ae2
Francesco <cesaranofrancescomain@gmail.com>
2022-12-22 14:07:06 +0100

    creata struttura base appFooter

---
+++ src/components/appFooter.vue
@@ -0,0 +1,34 @@
+<script >
+import topFoot from './topFoot.vue'
+
+export default {
+
+    name: "AppFooter",
+    components: {
+        topFoot
+    }
+
+}
+
+
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <!-- topFoot  -->
+        <topFoot />
+        <!-- botFoot  -->
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables.scss" as *;
+
+.wrapper {
+    padding: 16px 0;
+    background-color: $lima ;
+    height: 600px;
+}
+</style>
