03c8d9d30033daaae6cde7e2dce30fcc81a4624e
Francesco <cesaranofrancescomain@gmail.com>
2022-12-23 09:15:44 +0100

    aggiustato lo stile

--- src/components/FreeQuote.vue
+++ src/components/FreeQuote.vue
@@ -61,6 +61,32 @@ export default {
         background-color: $lima;
         border: 14px solid $border;
         color: $white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        text-align: center;
+
+        h2 {
+            font-weight: 700;
+            font-size: 3rem;
+        }
+
+        h4 {
+            font-size: 1rem;
+            font-weight: 700;
+        }
+
+        .line {
+            border-bottom: $white solid 1px;
+            width: 350px;
+            margin: 20px 0;
+        }
+
+        p {
+            width: 400px;
+            margin: 20px 0;
+        }
     }
 }
 </style>
